5fa5f1fbc431a3191867b13da883f3aec81d790b
webdesign <jannel.esilverconnect@gmail.com>
2018-06-26 18:27:41 +0800

    Added table.php

---
+++ table.php
@@ -0,0 +1,177 @@
+<table>
+<tr>
+	<td>Banks</td>
+	<td>
+		<p>DATCU - Corinth, TX</p>
+		<p>DATCU - Denton, TX</p>
+		<p>DATCU - Duchess St</p>
+		<p>Wells Fargo Office Remodel – Denton, TX</p>
+</td>
+</tr>
+<tr>
+	<td>Carwash</td>
+	<td>
+		<p>Appleseed Carwash Garland, TX</p>
+		<p>Appleseed Carwash Rowlett, TX</p>
+		<p>Sherman Carwash</p>
+		<p>Skyline Carwash</p>
+	</td>
+</tr>
+<tr>
+	<td>Charter Schools</td>
+	<td>
+		<ul>
+		<li>International Leadership of Texas – Grand Prairie</li>
+		<li>International Leadership of Texas - Keller</li>
+		<li>International Leadership of Texas – Orem</li>
+		<li>International Leadership of Texas – E. Fort Worth</li>
+		</ul>
+		
+	</td>
+</tr>
+<tr>
+	<td>Churches</td>
+	<td>
+		<ul>
+		<li>Denton Bible Church Student Ministry Center</p></li>
+		<li>Denton Bible Church Student Ministry Gym</p></li>
+		<li>Lamb Of God Lutheran Church</p></li>
+		<li>Northview Baptist Church</p></li>
+		<li>River of Life Cowboy Church</p></li>
+		</ul>
+		
+	</td>
+</tr>
+<tr>
+	<td>Movie Theater Remodels</td>
+	<td>
+		<ul>
+		<li>Cinemark ATC Storage</li>
+		<li>Aurora Century 16</li>
+		<li>AMC Willowbrook 24</li>
+		<li>Movie Tavern Aurora, CO</li>
+		<li>Cinemark #301</li>
+		<li>Cinemark XD 14 Alliance Town Center</li>
+		<li>Cinemark XD Addition Mansfield, TX</li>
+		<li>AMC Mesquite, TX</li>
+		<li>AMC Irving, TX</li>
+		<li>Century 16 CUSA#428</li>
+		<li>Cinemark #301</li>
+		<li>Cinemark #349</li>
+		<li>Cinemark Theater #3</li>
+		<li>Cinemark Theater #4</li>
+		<li>Cinemark Theater #9 </li>
+		<li>Cinemark Theater #6</li>
+		<li>Cinemark Frisco, TX</li>
+		<li>AMC Irving Mall 14</li>
+		<li>Cinemark – Warehouse Frisco</li>
+		<li>Cinemark XD #419 Remodel</li>
+		<li>Inwood Theater</li>
+		<li>Cinemark Oklahoma City, OK XD Remodel</li>
+		<li>Wylie 12 Wylie, TX</li>
+		<li>Cinemark NOC Frisco</li>
+		<li>Cinemark 56 – Grand Prairie</li>
+		<li>Cinemark Corp Office</li>
+		<li>Cinemark Oklahoma City, OK</li>
+		<li>Cinemark Oklahoma City, OK Aud. 14 PHII</li>
+		<li>Cinemark Oklahoma City, OK XD Remodel</li>
+		<li>Cinemark #218 Houston 290 XD</li>
+		<li>AMC Palace 9 Remodel</li>
+		<li>AMC Palace Reseating & Interior Refresh</li>
+		<li>Movie Tavern Bedford</li>
+		<li>Century 16 CUSA #428</li>
+		<li>Cinemark 17 IMAX</li>
+		<li>Cinemark Rave 13 Ridgemar Mall</li>
+		<li>Cinemark 15 Vista Ridge Mall</li>
+		<li>Cinemark Legacy #251 Plano</li>
+		<li>Cinemark Webb Chapel Restroom Remodel</li>
+		<li>Coyote Drive In</li>
+		<li>AMC Firewheel 18</li>
+		<li>AMC First Colony 24</li>
+		<li>AMC Penn Square</li>
+		<li>Cinemark #370 Allen 16</li>
+		<li>Cinemark Bar The Woodlands TX#216</li>
+		<li>AMC Katy Mills 20</li>
+		<li>AMC Mesquite 30</li>
+		<li>AMC Northpark Aud. Entry</li>
+		<li>AMC Northpark 15 – Aud. 8</li>
+		<li>AMC Parks at Arlington 18</li>
+		<li>AMC Gulf Pointe 30</li>
+		<li>AMC Hulen 10</li>
+		<li>AMC Barton Creek 14</li>
+		<li>AMC Galaxy 16 Aud. #9</li>
+		<li>Movie Tavern Bedford ADA</li>
+		<li>AMC Deerbrook 24 Aud. #7</li>
+		<li>AMC Stonebriar 24</li>
+		<li>AMC Willowbrook 24 Aud. 13 & 14</li>
+		<li>Cinemark Allen Kids Club</li>
+		<li>AMC Clearfork 8</li>
+		<li>Harkins Arvada Colorado</li>
+		<li>AMC Willowbrook Aud. #9 Entry</li>
+		<li>AMC Willowbrook Aud. #12 Entry</li>
+		<li>AMC Willowbrook Aud. #13 Entry</li>
+		</ul>
+	</td>
+</tr>
+<tr>
+	<td>Professional/Office</td>
+	<td>
+		<ul>
+		<li>Navo Retail Center</p></li>
+		<li>Anytime Fitness Lantana</p></li>
+		<li>Imagery In Motion Dance Studio</p></li>
+		<li>Little Guys Movers</p></li>
+		<li>Sudden Link Tioga Warehouse</p></li>
+		<li>Quality Industries</p></li>
+		<li>Tetra Pak – Various Jobs</p></li>
+		<li>Western Gate</p></li>
+		<li>Worlds of Wow </p></li>
+		</ul>
+	</td>
+</tr>
+<tr>
+	<td>Medical</td>
+	<td>
+		<ul>
+		<li>Apex Dental Nacogdoches, TX</li>
+		<li>Excel ER – Midland, TX</li>
+		<li>First Choice ER – Commerce City, CO</li>
+		<li>First Choice ER – Conroe, TX</li>
+		<li>First Choice ER – Fairfield, TX</li>
+		<li>First Choice ER – Summerwood</li>
+		<li>First Choice ER – Katy, TX</li>
+		<li>First Choice ER – McKinney, TX</li>
+		<li>First Choice ER – Meadow Grass, CO</li>
+		<li>First Choice ER – Richmond, TX</li>
+		<li>Goodwater Animal Hospital – Georgetown, TX</li>
+		<li>Krum Veterinary Clinic – Krum, TX</li>
+		<li>Modern Dentistry - College Station, TX</li>
+		<li>Ochsner Emergency Room – Marrero, LA</li>
+		<li>Smile Magic Dental – Lewisville, TX</li>
+		<li>Smile Magic Dental – Garland, TX</li>
+		<li>Veterinary Eye Institute – Dallas, TX</li>
+		</ul>
+	</td>
+</tr>
+<tr>
+	<td>Restaurants</td>
+	<td>
+		<ul>
+		<li>Chuy’s – Southlake, TX</li>
+		<li>McDonalds - Garland, TX</li>
+		<li>Olive Garden – Dallas, TX</li>
+		<li>Olive Garden – Irving, TX</li>
+		<li>Olive Garden – Lewisville, TX</li>
+		<li>Olive Garden – Mesquite, TX</li>
+		<li>Olive Garden – Arlington, TX</li>
+		<li>Olive Garden – Hulen, TX</li>
+		<li>Olive Garden – Ridgmar, TX</li>
+		<li>Peter Piper Pizza – Mesquite, TX</li>
+		<li>Pi Squared - Denton, TX</li>
+		<li>Taco Bueno – Moore, OK</li>
+		<li>Taco Bueno – Southlake, TX</li>
+		<li>Uchi Café & Bar  - Dallas, TX</li>
+		</ul>
+	</td>
+</tr>
+</table>
